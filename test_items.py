52e1773026f82fc602a6fd3be14adb9dccac0cec
PavMal-test <83552949+PavMal-test@users.noreply.github.com>
2021-06-17 22:04:29 +0300

    Added test_items file
    For test run with pytest

---
+++ test_items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:59:15 2021
+
+@author: user
+"""
+
+import time
+
+
+
+
+def test_guest_should_see_add_to_basket_button(browser):
+    
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    print(link)
+    browser.get(link)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
+    time.sleep(3)
