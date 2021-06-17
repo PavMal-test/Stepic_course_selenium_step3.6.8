0c0323ce2dea982cccafd4938fd0abd21fe59c57
PavMal-test <83552949+PavMal-test@users.noreply.github.com>
2021-06-17 22:28:55 +0300

    Added assertion, deleted print(url) command

--- test_items.py
+++ test_items.py
@@ -13,7 +13,6 @@ import time
 def test_guest_should_see_add_to_basket_button(browser):
     
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    print(link)
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert browser.find_element_by_css_selector(".btn-add-to-basket")
     time.sleep(3)
